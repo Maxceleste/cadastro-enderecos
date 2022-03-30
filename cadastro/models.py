7950df096e3a4ecb348f87ff262281713f3e04a8
Max Paulo <maxpauloteixeira@gmail.com>
2022-03-30 15:31:20 -0300

    criando models e migrando

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,14 @@
+from xml.parsers.expat import model
 from django.db import models
 
 # Create your models here.
+
+class enderecos(models.Model):
+    cep = models.CharField(max_length = 8)
+    endereco = models.TextField()
+    numero = models.CharField(max_length=50)
+    complemento = models.TextField()
+    bairro = models.TextField()
+    cidade = models.CharField(max_length=50)
+    uf = models.CharField(max_length=2)
+    descricao = models.TextField()
